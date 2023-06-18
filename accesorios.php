56c772933784802cecf277ac4c8ea89b79d12275
Jamiel Jackson <87439263+jamibj2000@users.noreply.github.com>
2023-06-18 16:49:22 -0500

    Add files via upload

---
+++ accesorios.php
@@ -0,0 +1,32 @@
+<?php
+$origin = $_SERVER['HTTP_ORIGIN'];
+require "access.php";
+
+$APIData = [];
+
+$image = [
+    "img/CrosbyA1328.jpg",
+];
+
+$code = [
+    "CrosbyA1328",
+];
+
+$buttonTitle = [
+    "Gancho acortador A-1328",
+];
+
+$cardTitle = [
+    "Gancho acortador Crosby A-1328",
+];
+
+for ($i = 0; $i < count($image); $i++) {
+    array_push($APIData, [
+        "image" => $image[$i],
+        "code" => $code[$i],
+        "buttonTitle" => $buttonTitle[$i],
+        "title" => $cardTitle[$i],
+    ]);
+}
+
+echo json_encode($APIData);
